2582490b16c511c5034d7de3a4eb6a908fbcae1b
Brennan Willingham <brennanw@Brennan-Willinghams-MacBook-Pro.local>
2015-02-06 11:43:59 -0600

    added total test

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -13,6 +13,23 @@ describe('hello', function () {
   });
 });
 
+describe('totalStocks', function () {
+        it('should return a set of the LastPrices', function () {
+          var stocks1 = [
+            { Symbol: 'AAPL', LastPrice: 12.45 },
+            { Symbol: 'MCSK', LastPrice: 23.56 }
+          ];
+
+          var stocks2 = [
+            { Symbol: 'Banana', LastPrice: 0.1 },
+            { Symbol: 'fred', LastPrice: 0.2 }
+          ];
+
+          totalStocks(stocks1).should.be.closeTo(36.01, 0.01);
+          totalStocks(stocks2).should.be.closeTo(0.3, 0.01);
+        });
+});
+
 describe('DOM', function () {
   describe('table', function () {
     beforeEach(function () {
